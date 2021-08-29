51075c117071cb9bce7bb1b9454382a0914cdaa3
Muhammadaziz <muhammadazizravshanbekov107@gmail.com>
2021-08-29 09:21:19 +0500

    Add and remove other files

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+	return <header>Header</header>;
+}
+
+function Footer() {
+	return <header>Footer</header>;
+}
+
+export { Header, Footer };
