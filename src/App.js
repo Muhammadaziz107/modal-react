d5cc8fb61c17341b9a163be62b16b83f44306f33
Muhammadaziz <muhammadazizravshanbekov107@gmail.com>
2021-08-29 10:07:17 +0500

    Add modal inner and modal--active

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./App.css";
 
 function App() {
+  const elMOdal = useRef(null);
+  const elInnerModal = useRef(null);
+  const elModalBtn = useRef(null);
 
   return (
-    <div>
-    
+    <div className="section">
+      <button
+        className="modal-btn"
+        ref={elModalBtn}
+        onClick={evt => {
+          elMOdal.current.classList.add("modal--active");
+        }}
+      >
+        Open
+      </button>
+      <div
+        className="modal"
+        ref={elMOdal}
+        onClick={evt => {
+          elMOdal.current.closest("div").classList.remove("modal--active");
+        }}
+      >
+        <div className="inner-modal" ref={elInnerModal}>
+          <h2>Title</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum, ipsum
+            voluptate. Ipsum obcaecati delectus minima consectetur aspernatur hic amet
+            dicta.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
